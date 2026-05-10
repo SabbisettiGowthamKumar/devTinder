a208c072327f914a4225415d623a03e12659cb8d
GowthamCC <sabbisetti.gowthamkumar@gmail.com>
2026-05-10 22:45:15 +0530

    exploring next() and multiple route handlers

--- src/app.js
+++ src/app.js
@@ -1,37 +1,35 @@
 const express = require("express");
 const app = express();
 
-// get will match only the get http request method
-app.get("/user", (req, res) => {
-  res.send({ firstname: "gowtham", lastname: "kumar" });
+// multiple route handlers
+app.get(
+  "/user",
+  [
+    (req, res, next) => {
+      console.log("first callback");
+      next();
+      //res.send({ firstname: "gowtham", lastname: "kumar" });
+    },
+    (req, res, next) => {
+      console.log("second callback");
+      next();
+    },
+  ],
+  (req, res) => {
+    console.log("third callback");
+    res.send({ firstname: "gowtham3", lastname: "kumar3" });
+  },
+);
+
+// another route handler technique
+app.get("/user2", (req, res, next) => {
+  console.log("user id is ");
+  next();
 });
 
-// ? means the preceding character is optional, so it will match both /abc and /ac
-app.get(/\/a(b)?c/, (req, res) => {
-  res.send({ firstname: "gowtham", lastname: "kumar" });
-});
-
-// :userID and :password are route parameters, it will match any value in that position and make it available in req.params
-app.get("/user/:userID/:password", (req, res) => {
-  console.log(req.params);
-  res.send({ firstname: "gowtham123", lastname: "kumar123" });
-});
-
-// post will match only the post http request method
-app.post("/user", (req, res) => {
-  console.log("data received from client");
-  res.send("post request received");
-});
-
-// put will match only the put http request method
-app.delete("/user", (req, res) => {
-  console.log("delete request received");
-  res.send("delete request received");
-});
-
-// use will match all the http request methods (get, post, put, delete)
-app.use("/test", (req, res) => {
-  res.send("hello from server");
+app.get("/user2", (req, res) => {
+  console.log("user2 route handler");
+  res.send({ firstname: "gowtham2", lastname: "kumar2" });
 });
 
 app.listen(7777, () => {

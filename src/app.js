f64063b258933c00e79d31e1795b4bbe191800e1
GowthamCC <sabbisetti.gowthamkumar@gmail.com>
2026-05-10 18:54:32 +0530

    exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("hello from dashboard");
+// get will match only the get http request method
+app.get("/user", (req, res) => {
+  res.send({ firstname: "gowtham", lastname: "kumar" });
 });
 
-app.use("/hello", (req, res) => {
+// post will match only the post http request method
+app.post("/user", (req, res) => {
+  console.log("data received from client");
+  res.send("post request received");
+});
+
+// put will match only the put http request method
+app.delete("/user", (req, res) => {
+  console.log("delete request received");
+  res.send("delete request received");
+});
+
+// use will match all the http request methods (get, post, put, delete)
+app.use("/test", (req, res) => {
   res.send("hello from server");
 });
-app.use("/4hello", (req, res) => {
-  res.send("hello hello hello hello");
-});
+
 app.listen(7777, () => {
   console.log("server is running on port 7777");
 });

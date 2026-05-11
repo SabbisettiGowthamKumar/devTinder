fe81871cdda1f13bdff76511a14a4bfc1f716c45
GowthamCC <sabbisetti.gowthamkumar@gmail.com>
2026-05-11 17:00:19 +0530

    CRUD operations with APIs

--- src/app.js
+++ src/app.js
@@ -20,6 +20,48 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+app.get("/getUser", async (req, res) => {
+  try {
+    const users = await User.find({ emailId: req.body.emailId });
+    res.send(users);
+  } catch (err) {
+    console.error("Error fetching users", err);
+    res.status(404).send("Something went wrong");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const allUsers = await User.find({});
+    res.send(allUsers);
+  } catch (err) {
+    console.error("Error fetching users", err);
+    res.status(404).send("Something went wrong");
+  }
+});
+
+app.delete("/deleteUser", async (req, res) => {
+  try {
+    const userID = req.body.userID;
+    await User.findByIdAndDelete(userID);
+    res.send("User deleted successfully");
+  } catch (err) {
+    console.error("Error deleting user", err);
+    res.status(404).send("Something went wrong");
+  }
+});
+
+app.patch("/updateUser", async (req, res) => {
+  try {
+    const userID = req.body.userID;
+    await User.findByIdAndUpdate(userID, req.body);
+    res.send("User updated successfully");
+  } catch (err) {
+    console.error("Error deleting user", err);
+    res.status(404).send("Something went wrong");
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("connected to database");
